44625a827fec1d61c2b903da12145b7fe708095a
mehmetpekmezci <mehmet.pekmezci.32@gmail.com>
2025-04-11 00:22:20 +0300

    MP

--- 03.data_generation/rir-reports/GTURIR/microphoneAnalysis.py
+++ 03.data_generation/rir-reports/GTURIR/microphoneAnalysis.py
@@ -21,6 +21,8 @@ import librosa
 
 import random
 
+import matplotlib.pyplot as plt
+
 
 np.set_printoptions(threshold=sys.maxsize)
 
@@ -70,8 +72,7 @@ def getLocalArgMax(limit,data):
      return np.argmax(data[:limit]>=maximum_value)
 
 
-def allignVertically(self,generated_data,real_data):
-#         return generated_data,real_data
+def allignVertically(generated_data,real_data):
          generated_data_max=np.max(np.abs(generated_data))
          real_data_max=np.max(np.abs(real_data))
          generated_data=generated_data/generated_data_max
@@ -127,7 +128,31 @@ def allignHorizontally(generated_data,real_data):
          # test edildi problem yok :)
          return generated_data,real_data
          
+def plotWav(real_data,generated_data,MSE,SSIM,glitch_points,mics,show=False,saveToPath=None):
+     plt.clf()
+     minValue=np.min(real_data)
+     minValue2=np.min(generated_data)
+     if minValue2 < minValue:
+        minValue=minValue2
 
+     plt.text(2600, minValue+abs(minValue)/11, f"MSE={float(MSE):.4f}\nSSIM={float(SSIM):.4f}\nGLITCH={int(len(glitch_points))}\nMICS={mics}", style='italic',
+        bbox={'facecolor': 'gray', 'alpha': 0.5, 'pad': 10})
+
+
+     plt.plot(real_data,color='#101010', label='real_data')
+     plt.plot(generated_data,color='#909090', label='generated_data')
+     plt.xlabel('Time')
+     plt.ylabel('Amlpitude')
+     plt.legend(loc = "upper right")
+
+     x=glitch_points
+     y=generated_data[x]
+     plt.scatter(x,y,color="black")
+
+     if show :
+        plt.show()
+     if saveToPath is not None:
+        plt.savefig(saveToPath)
          
          
 
@@ -155,9 +180,22 @@ for selectedRoomId in list_of_room_ids:
 
     #print("DENEME START")
     #for key,value in micPairSimilarities.items():
-    #   print(f"{key} : MSE: {np.mean(np.array(value['MSE']))} , SSIM: {np.mean(np.array(value['SSIM']))}, GLITCH: {np.mean(np.array(value['GLITCH']))}")
+    #   print(f"{key} : MSE: {np.mean(np.array(value['MSE']))} ({len(value['MSE'])}), SSIM: {np.mean(np.array(value['SSIM']))}, GLITCH: {np.mean(np.array(value['GLITCH']))}")
     #print("DENEME END")
 
+    maxSSIM=-100
+    maxSSIMRecord=[]
+    minSSIM=100
+    minSSIMRecord=[]
+    maxMSE=-100
+    maxMSERecord=[]
+    minMSE=100
+    minMSERecord=[]
+    maxGlitch=0
+    maxGlitchRecord=[]
+    minGlitch=10000
+    minGlitchRecord=[]
+    
     for dataline1 in room_data:
        speakerMotorIterationNo_1=dataline1[rir_data_field_numbers['speakerMotorIterationNo']]
        microphoneMotorIterationNo_1=dataline1[rir_data_field_numbers['microphoneMotorIterationNo']]
@@ -186,7 +224,7 @@ for selectedRoomId in list_of_room_ids:
            speakerCoordinatesY_2=float(dataline2[rir_data_field_numbers['speakerStandInitialCoordinateY']]) +float(dataline2[rir_data_field_numbers['speakerRelativeCoordinateY']])  
            speakerCoordinatesZ_2=float(dataline2[rir_data_field_numbers['speakerRelativeCoordinateZ']])  
 
-           DELTA=10
+           DELTA=8
            if micNo_1!=micNo_2:
               if  ( 
                       abs(microphoneCoordinatesX_1-microphoneCoordinatesX_2)<DELTA and  ## DELTA in  CM
@@ -195,39 +233,48 @@ for selectedRoomId in list_of_room_ids:
                       abs(speakerCoordinatesX_1-speakerCoordinatesX_2)<DELTA and
                       abs(speakerCoordinatesY_1-speakerCoordinatesY_2)<DELTA and
                       abs(speakerCoordinatesZ_1-speakerCoordinatesZ_2)<DELTA 
-                  ):
-                  print (f"(spkItrNo-micItrNo-SpkNo-micNo)  {speakerMotorIterationNo_1}-{microphoneMotorIterationNo_1}-{physicalSpeakerNo_1}-{micNo_1}  and {speakerMotorIterationNo_2}-{microphoneMotorIterationNo_2}-{physicalSpeakerNo_2}-{micNo_2} are close : ")
-                  print (f"mic_XYZ_Diff={abs(microphoneCoordinatesX_1-microphoneCoordinatesX_2)}-{abs(microphoneCoordinatesY_1-microphoneCoordinatesY_2)}-{abs(microphoneCoordinatesZ_1-microphoneCoordinatesZ_2)}  spk_XYZ_Diff={abs(speakerCoordinatesX_1-speakerCoordinatesX_2)}-{abs(speakerCoordinatesY_1-speakerCoordinatesY_2)}-{abs(speakerCoordinatesZ_1-speakerCoordinatesZ_2)}")
+                  ) :
+                  #print (f"(spkItrNo-micItrNo-SpkNo-micNo)  {speakerMotorIterationNo_1}-{microphoneMotorIterationNo_1}-{physicalSpeakerNo_1}-{micNo_1}  and {speakerMotorIterationNo_2}-{microphoneMotorIterationNo_2}-{physicalSpeakerNo_2}-{micNo_2} are close : ")
+                  #print (f"mic_XYZ_Diff={abs(microphoneCoordinatesX_1-microphoneCoordinatesX_2)}-{abs(microphoneCoordinatesY_1-microphoneCoordinatesY_2)}-{abs(microphoneCoordinatesZ_1-microphoneCoordinatesZ_2)}  spk_XYZ_Diff={abs(speakerCoordinatesX_1-speakerCoordinatesX_2)}-{abs(speakerCoordinatesY_1-speakerCoordinatesY_2)}-{abs(speakerCoordinatesZ_1-speakerCoordinatesZ_2)}")
 
                   mD ={}
+                  diffRecordName=f"{selectedRoomId}-{speakerMotorIterationNo_1}-{microphoneMotorIterationNo_1}-{physicalSpeakerNo_1}-{micNo_1}-{speakerMotorIterationNo_2}-{microphoneMotorIterationNo_2}-{physicalSpeakerNo_2}-{micNo_2}"
                   #micDiff[f"{selectedRoomId}-{speakerMotorIterationNo_1}-{microphoneMotorIterationNo_1}-{physicalSpeakerNo_1}-{micNo_1}-{speakerMotorIterationNo_2}-{microphoneMotorIterationNo_2}-{physicalSpeakerNo_2}-{micNo_2}"]={}
                   #mD = micDiff[f"{selectedRoomId}-{speakerMotorIterationNo_1}-{microphoneMotorIterationNo_1}-{physicalSpeakerNo_1}-{micNo_1}-{speakerMotorIterationNo_2}-{microphoneMotorIterationNo_2}-{physicalSpeakerNo_2}-{micNo_2}"]
                   
-                  rir_data_1=dataline1[rir_data_field_numbers['rirData']]
-                  rir_data_2=dataline2[rir_data_field_numbers['rirData']]
+                  rir_data_1=librosa.resample(dataline1[rir_data_field_numbers['rirData']], orig_sr=44100, target_sr=16000).astype(np.float32)
+                  rir_data_2=librosa.resample(dataline2[rir_data_field_numbers['rirData']], orig_sr=44100, target_sr=16000).astype(np.float32)
 
-                  #rir_data_1,rir_data_2=allignHorizontally(rir_data_1,rir_data_2)         
+                  rir_data_1=rir_data_1[0:3500]
+                  rir_data_2=rir_data_2[0:3500]
 
-                  #generated_data,real_data=self.allignVertically(generated_data,real_data)         
+                  rir_data_1,rir_data_2=allignHorizontally(rir_data_1,rir_data_2)         
+
+                  rir_data_1,rir_data_2=allignVertically(rir_data_1,rir_data_2)         
                   ######### BEGIN : YATAY ESITLEME (dikey esitleme zaten maksimum noktalarini esitliyerek yapilmisti)
                   #generated_data=generated_data-np.sum(generated_data)/generated_data.shape[0]
                   ## bu sekilde ortalamasi 0'a denk gelecek
                   ######### END: YATAY ESITLEME (dikey esitleme zaten maksimum noktalarini esitliyerek yapilmisti)
 
-                  rt60_1=dataline1[rir_data_field_numbers['rt60']]
-                  rt60_2=dataline2[rir_data_field_numbers['rt60']]
+                  #rt60_1=dataline1[rir_data_field_numbers['rt60']]
+                  #rt60_2=dataline2[rir_data_field_numbers['rt60']]
 
-                  mD["rt60"]=abs(rt60_1-rt60_2)
-                  mD["mse"]=np.square(np.subtract(rir_data_1,rir_data_2)).mean()
+                  #mD["rt60"]=abs(rt60_1-rt60_2)
+                  MSE=np.square(np.subtract(rir_data_1,rir_data_2)).mean()
+                  mD["mse"]=MSE
 
                   rir_data_1_tiled=np.tile(rir_data_1, (2, 1)) ## duplicate 1d data to 2d
                   rir_data_2_tiled=np.tile(rir_data_2, (2, 1)) ## duplicate 1d data to 2d
                   rir_data_1_tiled=np.reshape(rir_data_1_tiled,(1,1,rir_data_1_tiled.shape[0],rir_data_1_tiled.shape[1]))
                   rir_data_2_tiled=np.reshape(rir_data_2_tiled,(1,1,rir_data_2_tiled.shape[0],rir_data_2_tiled.shape[1]))
-                  rir_data_1_tensor=torch.from_numpy(rir_data_1_tiled)
-                  rir_data_2_tensor=torch.from_numpy(rir_data_2_tiled)
-                  mD["ssim"]=ssim(rir_data_1_tensor,rir_data_2_tensor,data_range=4.0,size_average=True).item()
-                  mD["glitch_point_count"]=len(getGlitchPoints(rir_data_1,rir_data_2))
+                  rir_data_1_tensor=torch.from_numpy(rir_data_1_tiled).to(torch.float)
+                  rir_data_2_tensor=torch.from_numpy(rir_data_2_tiled).to(torch.float)
+                  #print(rir_data_1_tensor)
+                  #print(rir_data_2_tensor)
+                  SSIM=ssim(rir_data_1_tensor,rir_data_2_tensor,data_range=4.0,size_average=True).item()
+                  mD["ssim"]=SSIM
+                  glitch_points=getGlitchPoints(rir_data_1,rir_data_2)
+                  mD["glitch_point_count"]=len(glitch_points)
                   if micNo_1 < micNo_2:
                      mD["mics"]=f"{micNo_1}-{micNo_2}"
                   else:
@@ -243,8 +290,80 @@ for selectedRoomId in list_of_room_ids:
                   micPairSimilarities[mD["mics"]]["SSIM"].append(mD["ssim"])
                   micPairSimilarities[mD["mics"]]["GLITCH"].append(mD["glitch_point_count"])
 
-                  print(mD)
+                  own_record=[rir_data_1,rir_data_2,MSE,SSIM,glitch_points,mD["mics"]]
 
+                  if maxSSIM < mD["ssim"]:
+                     maxSSIM=mD["ssim"]
+                     maxSSIMRecord=own_record
+                     #print("maxSSIM")
+                     #print(own_record)
+
+                  elif minSSIM > mD["ssim"]:
+                     minSSIM=mD["ssim"]
+                     minSSIMRecord=own_record
+                     #print("minSSIM")
+                     #print(own_record)
+
+                  if maxMSE < mD["mse"]:
+                     maxMSE=mD["mse"]
+                     maxMSERecord=own_record
+                     #print("maxMSE")
+                     #print(own_record)
+
+                  elif minMSE > mD["mse"]:
+                     minMSE=mD["mse"]
+                     minMSERecord=own_record
+                     #print("minMSE")
+                     #print(own_record)
+
+                  if maxGlitch < mD["glitch_point_count"]:
+                     maxGlitch=mD["glitch_point_count"]
+                     maxGlitchRecord=own_record
+                     #print("maxGlitch")
+                     #print(own_record)
+
+                  elif minMSE > mD["glitch_point_count"]:
+                     minGlitch=mD["glitch_point_count"]
+                     minGlitchRecord=own_record
+                     #print("minGlitch")
+                     #print(own_record)
+
+                  #plotWav(rir_data_1,rir_data_2,MSE,SSIM,glitch_points,mD["mics"],show=True)
+
+                  #print(mD)
+
+output=""
 for key,value in micPairSimilarities.items():
-       print(f"{key} : MSE: {np.mean(np.array(value['MSE']))} , SSIM: {np.mean(np.array(value['SSIM']))}, GLITCH: {np.mean(np.array(value['GLITCH']))}")
+       output=f"{key} : MSE: {np.mean(np.array(value['MSE']))} , SSIM: {np.mean(np.array(value['SSIM']))}, GLITCH: {np.mean(np.array(value['GLITCH']))} \n"+output
+
+with open('output.txt', 'w') as f:
+    print(output, file=f)
+
+print(output)
+
+
+
+records=[]
+maxSSIMRecord.append("maxSSIM.png")
+records.append(maxSSIMRecord)
+minSSIMRecord.append("minSSIM.png")
+records.append(minSSIMRecord)
+maxMSERecord.append("maxMSE.png")
+records.append(maxMSERecord)
+minMSERecord.append("minMSE.png")
+records.append(minMSERecord)
+maxGlitchRecord.append("maxGlitch.png")
+records.append(maxGlitchRecord)
+minGlitchRecord.append("minGlitch.png")
+records.append(minGlitchRecord)
+
+for record_to_plot in records:
+    if len(record_to_plot)>5 :
+       print(record_to_plot[5])
+       print(record_to_plot[6])
+       plotWav(record_to_plot[0],record_to_plot[1],record_to_plot[2],record_to_plot[3],record_to_plot[4],record_to_plot[5],saveToPath=record_to_plot[6])
+    else:
+       print(record_to_plot[0])
+
+
 
