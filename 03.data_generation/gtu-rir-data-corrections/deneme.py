e97bcdbe1e2030177919803928e94d6e30f22c8b
Mehmet Pekmezci <mehmet.pekmezci.32@gmail.com>
2025-05-25 12:52:06 +0300

    MP

--- 03.data_generation/gtu-rir-data-corrections/deneme.py
+++ 03.data_generation/gtu-rir-data-corrections/deneme.py
@@ -1,7 +1,8 @@
 import trimesh
+import numpy as np
 
 m=trimesh.load_mesh('mesh.obj')
-Y=11.27/2
-X=5.42/2
+Y=np.max(m.vertices[:,2])/2
+X=np.max(m.vertices[:,0])/2
 m.apply_transform(trimesh.transformations.translation_matrix([-X,0,-Y]))
 m.export('mesh1.obj')
